a0a2f87a535385e66e3ccca134d5159b9010dc2f
Marco <marco.chianese93@gmail.com>
2020-11-01 23:59:33 +0100

    aggiunta utente oggetto in un array

--- script.js
+++ script.js
@@ -5,20 +5,36 @@
 // tramite 3 prompt, chiedere all'utente il nome, il cognome e l'età di un nuovo studente. Creare con questi dati un nuovo oggetto e inserirlo nell'array del punto precedente.
 
 
-var studente = [
+var studenti = [
 {
     nome : "Marco" ,
     cognome  : "chianese",
-
+    eta : 27,
 },
 {
     nome : "Sofia" ,
-    cognome : "Perlari"
+    cognome : "Perlari",
+    eta : 30 ,
 },
 ];
 
-
-for (var key in studente) {
-    console.log(studente);
-    console.log(studente[key]);
+// con questo ciclo vado ad estrarre i dati dei singoli oggetti
+for (var key in studenti) {
+    console.log(studenti);
+    console.log(studenti[key]);
 }
+
+// queste variabili andranno a rappresentare i valori che l'utente inserirá per aggiungere i suoi dati all'array degli studenti
+var  nome = prompt("inserisci il tuo nome ");
+var  cognome = prompt("inserisci il tuo cognome ");
+var eta = parseInt(prompt("Quanti anni hai")) ;
+
+var nuovo_studente = {
+    nome: nome ,
+    cognome : cognome ,
+    eta : eta ,
+};
+
+console.log(studenti);
+studenti.push(nuovo_studente);
+console.log(studenti[key]);
